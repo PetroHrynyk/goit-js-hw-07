af5f0c06eef8f7423ba76e90cc47a4f78c3a26d3
PetroHrynyk <grynyk82@gmail.com>
2022-07-28 19:06:05 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -18,3 +18,40 @@ function createGalleryMarkup(arr) {
 		})
 		.join("");
 }
+
+function onGalleryItemClick(event) {
+	event.preventDefault();
+
+	const isGalleryItemEl = event.target.classList.contains("gallery__image");
+	if (!isGalleryItemEl) {
+		return;
+	}
+
+	const galleryImage = event.target;
+	const galleryImageUrlBig = galleryImage.dataset.source;
+
+	onBasicLightbox(galleryImageUrlBig);
+}
+
+let instance = {};
+
+function onBasicLightbox(imgUrl) {
+	instance = basicLightbox.create(`<img src="${imgUrl}" width="800" height="600">`, {
+		onShow: () => {
+			window.addEventListener("keydown", onEscKeyPress);
+		},
+		onClose: () => {
+			window.removeEventListener("keydown", onEscKeyPress);
+		},
+	});
+
+	instance.show();
+}
+
+function onEscKeyPress(event) {
+	const ESC_KEY_CODE = "Escape";
+
+	if (event.code === ESC_KEY_CODE) {
+		instance.close();
+	}
+}
